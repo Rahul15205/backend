ac2bb0cce808e8c8d07ae9e95c7c42eb9c729a75
Rahul Kumar <rahulkumarshukla4627@gmail.com>
2024-08-28 12:45:51 +0530

    register controller for user

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constants.js"
 const connectDB = async () => {
     try {
         const connectionInstace = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`/n MONGODB connected : ${connectionInstace.connection.host}`);
+        console.log(`\n MONGODB connected : ${connectionInstace.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection failed",error);;
         process.exit(1);
